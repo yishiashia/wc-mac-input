3da568e0bb647cdae0b648f5f304fbd335eefdb8
yishiashia <yishiashia@gmail.com>
2022-07-14 10:45:26 +0800

    feat: add backspace keypress test cases

--- tests/units/wcMacInput.spec.js
+++ tests/units/wcMacInput.spec.js
@@ -5,26 +5,31 @@ import MacInput from '../../src/wcMacInput';
 describe('wcMacInput.js', () => {
 
   window.customElements.define('mac-input', MacInput);
+  document.body.innerHTML = `
+  <h1>Custom element test</h1>
+  <mac-input id="mac-input" name="mac"></mac-input>
+  `;
+  const customElement = document.getElementById('mac-input');
 
-  test('Render mac-input element with text type in JSDOM', () => {
+  let eventCounter = 0
 
-    let eventCounter = 0
-
-    document.body.innerHTML = `
-      <h1>Custom element test</h1>
-      <mac-input id="mac-input" name="mac"></mac-input>
-    `;
+  test('Render mac-input element in JSDOM', () => {
 
     // 1. Check shadow dom element is rendered
-    const customElement = document.getElementById('mac-input');
     expect(customElement === null).toBeFalsy();
+    // 2. Check input fields count to be 6
+    const inputElements = customElement.querySelectorAll('input')
+    expect(inputElements.length === 6)
 
+  });
+
+  test('Simulate keyin with mac-input element', () => {
     customElement.addEventListener('change', function(e) {
       // changeValue = e.target.value_
       eventCounter++;
     });
 
-    // 2. check valid input on inputs
+    // check valid input on inputs
     let inputEl;
     let result = '';
     let testCount = 0;
@@ -44,8 +49,14 @@ describe('wcMacInput.js', () => {
       expect(document.querySelector('input[name=mac]').value === result)
       expect(testCount === eventCounter)
     }
+  })
 
-    // 3. check valid input on input 1
+  test('Simulate invalid keyin with mac-input element', () => {
+    let inputEl;
+    let result = '';
+    let testCount = 0;
+
+    eventCounter = 0
     window.alert = function() {
       /* do nothing */
     }
@@ -65,6 +76,50 @@ describe('wcMacInput.js', () => {
       expect(document.querySelector('input[name=mac]').value === result)
       expect(testCount === eventCounter)
     }
+  });
+
+  test('Test backspace key press event', () => {
+    let inputSlots = []
+    inputSlots[0] = customElement.shadowRoot.querySelector('input:nth-child(1)')
+    inputSlots[1] = customElement.shadowRoot.querySelector('input:nth-child(2)')
+    inputSlots[2] = customElement.shadowRoot.querySelector('input:nth-child(3)')
+    inputSlots[3] = customElement.shadowRoot.querySelector('input:nth-child(4)')
+    inputSlots[4] = customElement.shadowRoot.querySelector('input:nth-child(5)')
+    inputSlots[5] = customElement.shadowRoot.querySelector('input:nth-child(6)')
+
+    inputSlots[0].value = "AA"
+    inputSlots[1].value = "AA"
+    inputSlots[2].value = "AA"
+    inputSlots[3].value = ""
+
+    fireEvent.keyDown(inputSlots[3], {
+      key: "Backspace",
+      code: "Backspace",
+      keyCode: 8,
+      charCode: 8
+    })
+    fireEvent.input(inputSlots[3], {
+      bubbles: true,
+      cancelable: true
+    })
+    expect(document.querySelector('input[name=mac]').value === "AA-AA-A---")
+  });
+
+  test('Remove event listener after disconnectedCallback is called', () => {
+    let events = {};
+    document.addEventListener = jest.fn((event, callback) => {
+      events[event] = callback;
+    });
+
+    document.removeEventListener = jest.fn((event, callback) => {
+      delete events[event];
+    });
+
+    document.body.innerHTML = `
+    <h1>Custom element test</h1>
+    <mac-input id="mac-input" name="mac"></mac-input>
+    `;
+    console.log(events);
   });
 
 });
